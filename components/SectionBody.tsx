a8fb716a4e9e0871212a26f946cfcc63fee8b541
@wwwjim <send@jimmy.email>
2023-03-06 20:19:31 -0800

    DataLine and DataQuote

--- components/SectionBody.tsx
+++ components/SectionBody.tsx
@@ -3,6 +3,25 @@
 import styles from '@components/SectionBody.module.scss';
 
 import * as React from 'react';
+import * as Utilities from '@common/utilities';
+
+const DataLine = (props) => {
+  return (
+    <span className={styles.dataTag}>
+      <span className={styles.dataTagLeft}>{props.children}</span>
+      <span className={styles.dataTagRight}>{props.label}</span>
+    </span>
+  );
+};
+
+const DataQuote = (props) => {
+  return (
+    <span className={styles.dataTag}>
+      <span className={styles.dataTagLeftQuote} />
+      <span className={styles.dataTagRightQuote}>{props.children}</span>
+    </span>
+  );
+};
 
 export default function SectionBody(props) {
   const [data, setData] = React.useState({
@@ -35,10 +54,41 @@ export default function SectionBody(props) {
   return (
     <div className={styles.body}>
       <p className={styles.paragraph}>
-        We're releasing a new tool <strong>March 27th, 2023</strong> that will help you onboard your data to a Filecoin Storage Provider. It could be yours, it could be anyone
-        elses!
+        We're releasing a new tool <strong>March 27th, 2023</strong> that will help you onboard your data to any Filecoin Storage Provider including yours.
       </p>
       <p className={styles.paragraph}>Here are our alpha testing results and performance.</p>
+
+      <div className={styles.performance}>
+        <DataLine label="Total end-to-end deals">{data.total_e2e_deals}</DataLine>
+        <DataQuote>
+          An end-to-end deal is when a user has a file, uses [any data prep service + any data client + any filecoin address], and knows that deal ends up on a storage provider,
+          sealed, with a receipt.
+        </DataQuote>
+        <DataLine label="Total data size in end-to-end deals">{Utilities.bytesToSize(data.total_e2e_deals_in_bytes)}</DataLine>
+
+        <DataLine label="Total import deals">{data.total_import_deals}</DataLine>
+        <DataQuote>
+          An import deal is when the Filecoin Network has some mechanism for expecting a certain CID (the analogy is like an Amazon S3 Pre-signed URL, expecting certain bytes) and
+          the import of those deals comes from any mechanism the operator decides to use.
+        </DataQuote>
+        <DataLine label="Total data size in import deals">{Utilities.bytesToSize(data.total_import_deals_in_bytes)}</DataLine>
+
+        <DataLine label="Total data onboarded to Filecoin">{Utilities.bytesToSize(data.total_e2e_deals_in_bytes + data.total_import_deals_in_bytes)}</DataLine>
+
+        <DataQuote>
+          We will always know our performance relative to the total storage onboarded onto Filecoin (
+          <a href="https://dashboard.starboard.ventures/market-deals" target="_blank">
+            https://dashboard.starboard.ventures/market-deals
+          </a>
+          ). Delta's goal is to perform at least 25% of the work for the Filecoin Storage Network by the end of 2023.
+        </DataQuote>
+
+        <DataLine label="What if we scaled Delta 1000x?">{Utilities.bytesToSize((data.total_e2e_deals_in_bytes + data.total_import_deals_in_bytes) * 1000)}</DataLine>
+
+        <DataLine label="How many storage providers?">{data.total_miners}</DataLine>
+
+        <DataLine label="Total piece commitment made">{data.total_piece_commitment_made}</DataLine>
+      </div>
     </div>
   );
 }
--- components/SectionFooter.tsx
+++ components/SectionFooter.tsx
@@ -5,19 +5,8 @@ import * as React from 'react';
 export default function SectionFooter(props) {
   return (
     <div className={styles.body}>
-      <a href="https://filecoin.io" className={styles.link} target="_blank">
-        ↗ Learn more about Filecoin
-      </a>
-      <a href="https://protocol.ai" className={styles.link} target="_blank">
-        ↗ Learn more about Protocol Labs
-      </a>
-
-      <a href="https://filecoin.io/slack" className={styles.link} target="_blank">
-        ↗ Want to work with the team? Join us on Slack in #ecosystem-dev.
-      </a>
-
-      <a href="https://estuary.tech" className={styles.link} target="_blank">
-        ↗ Do you need storage and the promise of retrievable data today? Learn more about Estuary
+      <a href="https://fw.services" className={styles.link} target="_blank">
+        ↗ Learn more about Filecoin Web Services
       </a>
     </div>
   );
