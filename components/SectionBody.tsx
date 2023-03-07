fe71ce09e2f9548f4eee91fc9d353b0df5b5b71c
@wwwjim <send@jimmy.email>
2023-03-06 19:50:09 -0800

    integrates endpoint

--- components/SectionBody.tsx
+++ components/SectionBody.tsx
@@ -1,45 +1,44 @@
+'use client';
+
 import styles from '@components/SectionBody.module.scss';
 
 import * as React from 'react';
 
 export default function SectionBody(props) {
+  const [data, setData] = React.useState({
+    total_content_consumed: 0,
+    total_e2e_deals: 0,
+    total_e2e_deals_in_bytes: 0,
+    total_import_deals: 0,
+    total_import_deals_in_bytes: 0,
+    total_miners: 0,
+    total_piece_commitment_made: 0,
+    total_piece_committed: 0,
+    total_proposal_made: 0,
+    total_proposal_sent: 0,
+    total_sealed_deal_in_bytes: 0,
+    total_storage_allocated: 0,
+    total_transfer_finished: 0,
+    total_transfer_started: 0,
+  });
+
+  React.useEffect(() => {
+    async function init() {
+      const response = await fetch('https://node.delta.store/open/stats/totals/info');
+      const json = await response.json();
+      setData({ ...json });
+    }
+
+    init();
+  }, []);
+
   return (
     <div className={styles.body}>
-      <p className={styles.paragraph}>Do you have a lot of data you want to put on Filecoin?</p>
-      <p className={styles.paragraph}>Are you having difficulty finding a tool you can use to get the job done?</p>
       <p className={styles.paragraph}>
-        We hear you loud and clear. We are building a tool (<i>∆ Delta</i>) where you can upload as much data as a storage provider will take.
+        We're releasing a new tool <strong>March 27th, 2023</strong> that will help you onboard your data to a Filecoin Storage Provider. It could be yours, it could be anyone
+        elses!
       </p>
-
-      <p className={styles.paragraph}>
-        We understand the strife that storage providers face. Being a storage provider is hard and they want to break even on their large investments. They need more QAP (
-        <a href="https://spec.filecoin.io/systems/filecoin_mining/sector/sector-quality/" target="_blank">
-          Quality Adjusted Power
-        </a>
-        ) to have a higher chance of earning{' '}
-        <a href="https://medium.com/cryptoeconlab/filecoins-adaptive-block-rewards-will-autonomously-reactivate-after-baseline-crossing-in-early-e5079bf16970" target="_blank">
-          Filecoin block rewards
-        </a>{' '}
-        for participating in the network. Everyone knows that storage-providers want a tool to onboard more data that is straight forward and does not require special hacks or
-        scripts. In addition, storage providers do not want to deal with people telling them what they can or cannot do.
-      </p>
-
-      <p className={styles.paragraph}>
-        Let us help everyone gain the competitive edge while giving everyone an opportunity to contribute to the source code of ∆ Delta to onboard more data to Filecoin.
-      </p>
-
-      <p className={styles.paragraph}>
-        Unlike <a href="https://estuary.tech">Estuary</a>, another tool for onboarding data and retrieving unsealed copies through IPFS,{' '}
-        <strong>∆ Delta is only focused on getting your data onto a storage provider in the Filecoin Network</strong>. ∆ Delta does not provide a means to retrieve data. ∆ Delta
-        will only provide you a receipt of where your data is once it is transferred and sealed.
-      </p>
-
-      <p className={styles.paragraph}>
-        You (<i>the data client</i>) can upload as many files as a storage provider can take, we recommend the size of your data is between 1GB to 32GB. ∆ Delta will then queue the
-        request for the content or CommP, then run a dispatcher to check the status of your storage deal until it is complete.
-      </p>
-
-      <p className={styles.paragraph}>Check here soon for updates! We look forward to helping you.</p>
+      <p className={styles.paragraph}>Here are our alpha testing results and performance.</p>
     </div>
   );
 }
